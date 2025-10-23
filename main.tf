8ce80436373647bc814f5cd455385116db65d42c
Naga <pavantonystark99@gmail.com>
2025-10-23 22:34:09 +0530

    passed vpc_tags

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "vpc" {
+  #below what we are sending is like calling a function with arguments
   source      = "../terrraform-aws-vpc-module"
   cidr_block  = var.cidr_block
   environment = var.environment
   project     = var.project
+  vpc_tags = var.vpc_tags
 
 }
