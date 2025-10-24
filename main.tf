4df6557a415b776a3cba05c82de3daf1ce242b3b
Naga <pavantonystark99@gmail.com>
2025-10-24 13:19:55 +0530

    route_table_tags_passed to module

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
 module "vpc" {
   #below what we are sending is like calling a function with arguments
-  source      = "../terrraform-aws-vpc-module"
-  cidr_block  = var.cidr_block
-  environment = var.environment
-  project     = var.project
-  vpc_tags = var.vpc_tags
-igw_tags = var.igw_tags
-public_subnet_tags=var.public_subnet_tags
-private_subnet_tags=var.private_subnet_tags
-database_subnet_tags=var.database_subnet_tags
+  source                  = "../terrraform-aws-vpc-module"
+  cidr_block              = var.cidr_block
+  environment             = var.environment
+  project                 = var.project
+  vpc_tags                = var.vpc_tags
+  igw_tags                = var.igw_tags
+  public_subnet_tags      = var.public_subnet_tags
+  private_subnet_tags     = var.private_subnet_tags
+  database_subnet_tags    = var.database_subnet_tags
+  route_table_tags_public = var.route_table_tags_public
+    route_table_tags_private = var.route_table_tags_private
+    route_table_tags_database = var.route_table_tags_database
 
-public-subnet-cidr-block = var.public-subnet-cidr-block
-private-subnet-cidr-block = var.private-subnet-cidr-block
-database-subnet-cidr-block = var.database-subnet-cidr-block
+  public-subnet-cidr-block   = var.public-subnet-cidr-block
+  private-subnet-cidr-block  = var.private-subnet-cidr-block
+  database-subnet-cidr-block = var.database-subnet-cidr-block
 }
 
 data "aws_availability_zones" "availables" {
--- variables.tf
+++ variables.tf
@@ -2,73 +2,97 @@ variable "cidr_block" {
   default = "10.0.0.0/16"
 }
 variable "environment" {
-  type = string
+  type    = string
   default = "dev"
 }
 
 variable "project" {
-  type = string
+  type    = string
   default = "roboshop"
 }
 
 #CIDR - loop creation 
 variable "public-subnet-cidr-block" {
-  type = list
+  type    = list(any)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
-  
+
 }
 
 variable "database-subnet-cidr-block" {
-  type = list
+  type    = list(any)
   default = ["10.0.11.0/24", "10.0.12.0/24"]
-  
+
 }
 
 variable "private-subnet-cidr-block" {
-  type = list
+  type    = list(any)
   default = ["10.0.21.0/24", "10.0.22.0/24"]
-  
+
 }
 variable "vpc_tags" {
   default = {
     enable_dns_hostnames = "added in module"
-    Terraform_User= true
+    Terraform_User       = true
   }
 }
 
 variable "igw_tags" {
   default = {
     added_ref_to_VpcId = true
-    Terraform_User= true
+    Terraform_User     = true
   }
 }
 
 variable "public_subnet_tags" {
   default = {
-    availability_zone = "added by slicing the output received from data sourcing - aws_availability_zones"
-    Terraform_User = true
+    availability_zone       = "added by slicing the output received from data sourcing - aws_availability_zones"
+    Terraform_User          = true
     map_public_ip_on_launch = true
-    subnet= "public"
+    subnet                  = "public"
 
   }
 }
 
 variable "private_subnet_tags" {
   default = {
-    availability_zone = "added by slicing the output received from data sourcing - aws_availability_zones"
-    Terraform_User = true
+    availability_zone       = "added by slicing the output received from data sourcing - aws_availability_zones"
+    Terraform_User          = true
     map_public_ip_on_launch = false
-    subnet= "private"
+    subnet                  = "private"
 
   }
 }
 
 variable "database_subnet_tags" {
   default = {
-    availability_zone = "added by slicing the output received from data sourcing - aws_availability_zones"
-    Terraform_User = true
+    availability_zone       = "added by slicing the output received from data sourcing - aws_availability_zones"
+    Terraform_User          = true
     map_public_ip_on_launch = false
-    subnet= "database"
+    subnet                  = "database"
 
+  }
+}
+
+variable "route_table_tags_public" {
+  type = map(any)
+  default = {
+    passed_only_vpc_id = true
+    type               = "public"
+  }
+}
+
+variable "route_table_tags_private" {
+  type = map(any)
+  default = {
+    passed_only_vpc_id = true
+    type               = "private"
+  }
+}
+
+variable "route_table_tags_database" {
+  type = map(any)
+  default = {
+    passed_only_vpc_id = true
+    type               = "database"
   }
 }
